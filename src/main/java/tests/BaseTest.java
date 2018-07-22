037b9611262ee1a818c0d378a63f4a0ead64f9dd
Artem <koryakin1993@inbox.ru>
2018-07-22 23:14:12 +0300

    refactoring

--- src/main/java/tests/BaseTest.java
+++ src/main/java/tests/BaseTest.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
